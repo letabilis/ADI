398b2b93eb3178dd1171188791fab1fc4cfd1748
letabilis <sahilvdowlani@outlook.com>
2025-04-26 19:21:53 -0400

    feat(UserController): add(DeleteMapping) deleteUserById() - Endpoint (/users/{userId})

--- src/main/java/spring/project/adi/controller/UserController.java
+++ src/main/java/spring/project/adi/controller/UserController.java
@@ -16,6 +16,7 @@ import java.util.UUID;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
@@ -52,5 +53,10 @@ public class UserController {
         
         return userDTO.isPresent() ? ResponseEntity.ok(userDTO) : ResponseEntity.notFound().build();
     }
-    
-}
+
+    @DeleteMapping("/{userId}")
+    public ResponseEntity<Void> deleteUserById(@PathVariable("userId") UUID userId) {
+        this.userService.deleteUserById(userId);
+        return ResponseEntity.noContent().build();
+    }
+}   
