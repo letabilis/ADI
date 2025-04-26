c81a2f83555f8436ae536b348a12ce671ba13eee
letabilis <sahilvdowlani@outlook.com>
2025-04-26 19:54:31 -0400

    feat(UserMapper): add map UpdateUserDTO -> UserEntity

--- src/main/java/spring/project/adi/mapper/UserMapper.java
+++ src/main/java/spring/project/adi/mapper/UserMapper.java
@@ -9,6 +9,7 @@ import org.mapstruct.ReportingPolicy;
 import spring.project.adi.model.User;
 import spring.project.adi.dto.CreateUserDTO;
 import spring.project.adi.dto.GetUserDTO;
+import spring.project.adi.dto.UpdateUserDTO;
 
 
 @Mapper(componentModel = "spring", unmappedTargetPolicy = ReportingPolicy.ERROR)
@@ -17,6 +18,12 @@ public interface UserMapper {
     @Mapping(target = "creationTimestamp", ignore = true)
     @Mapping(target = "updateTimestamp", ignore = true)
     User toEntity(CreateUserDTO createUserDTO);
+
+    @Mapping(target = "id", ignore = true)
+    @Mapping(target = "email", ignore= true)
+    @Mapping(target = "creationTimestamp", ignore = true)
+    @Mapping(target = "updateTimestamp", ignore = true)
+    User toEntity(UpdateUserDTO updateUserDTO);
 
     GetUserDTO toDTO(User user);
 
