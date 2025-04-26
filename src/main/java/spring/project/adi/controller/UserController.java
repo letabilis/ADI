19958945237b53aa66500d56b29a95dcb24b1618
letabilis <sahilvdowlani@outlook.com>
2025-04-26 15:16:16 -0400

    change(UserController): getUserById() RequestParam -> PathVariable Approach + ParameterName(id -> userId)

--- src/main/java/spring/project/adi/controller/UserController.java
+++ src/main/java/spring/project/adi/controller/UserController.java
@@ -16,7 +16,8 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.PathVariable;
+
 
 
 
@@ -36,9 +37,9 @@ public class UserController {
         return ResponseEntity.created(URI.create("/users/" + userId.toString())).build();
     }
     
-    @GetMapping
-    public ResponseEntity<Optional<GetUserDTO>> getUserById(@RequestParam UUID id) {
-        var userDTO = this.userService.getUserById(id);
+    @GetMapping("/{userId}")
+    public ResponseEntity<Optional<GetUserDTO>> getUserById(@PathVariable("userId") UUID userId) {
+        var userDTO = this.userService.getUserById(userId);
         
         return userDTO.isPresent() ? ResponseEntity.ok(userDTO) : ResponseEntity.notFound().build();
     }
