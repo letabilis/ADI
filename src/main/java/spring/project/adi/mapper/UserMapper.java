d39069f1f4237b288430ac583e2813d26410ec9f
letabilis <sahilvdowlani@outlook.com>
2025-04-26 19:59:34 -0400

    Revert "feat(UserMapper): add map UpdateUserDTO -> UserEntity"
    This reverts commit c81a2f83555f8436ae536b348a12ce671ba13eee.

--- src/main/java/spring/project/adi/mapper/UserMapper.java
+++ src/main/java/spring/project/adi/mapper/UserMapper.java
@@ -9,7 +9,6 @@ import org.mapstruct.ReportingPolicy;
 import spring.project.adi.model.User;
 import spring.project.adi.dto.CreateUserDTO;
 import spring.project.adi.dto.GetUserDTO;
-import spring.project.adi.dto.UpdateUserDTO;
 
 
 @Mapper(componentModel = "spring", unmappedTargetPolicy = ReportingPolicy.ERROR)
@@ -18,12 +17,6 @@ public interface UserMapper {
     @Mapping(target = "creationTimestamp", ignore = true)
     @Mapping(target = "updateTimestamp", ignore = true)
     User toEntity(CreateUserDTO createUserDTO);
-
-    @Mapping(target = "id", ignore = true)
-    @Mapping(target = "email", ignore= true)
-    @Mapping(target = "creationTimestamp", ignore = true)
-    @Mapping(target = "updateTimestamp", ignore = true)
-    User toEntity(UpdateUserDTO updateUserDTO);
 
     GetUserDTO toDTO(User user);
 
