4d1d64f4211b19f63493f8643f1e0146ac4a2c49
letabilis <sahilvdowlani@outlook.com>
2025-04-27 13:28:29 -0400

    add(UserServiceTest.java) Boilerplate

---
+++ src/test/java/spring/project/adi/service/UserServiceTest.java
@@ -0,0 +1,30 @@
+package spring.project.adi.service;
+
+import org.junit.jupiter.api.Test;
+
+public class UserServiceTest {
+    @Test
+    void testCreateUser() {
+
+    }
+
+    @Test
+    void testDeleteUserById() {
+
+    }
+
+    @Test
+    void testGetUserById() {
+
+    }
+
+    @Test
+    void testListUsers() {
+
+    }
+
+    @Test
+    void testUpdateUserById() {
+
+    }
+}
